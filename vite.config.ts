188fbb1ff6e2fd00c54f45688cf1cd89aeea52d3
Mohammed Shibil PV <muhammedshibilpv77@gmail.com>
2024-10-10 09:41:50 +0530

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['react-pdf'],
+//   },
+//   build: {
+//     commonjsOptions: {
+//       include: [/react-pdf/, /pdfjs-dist/],
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react-pdf'],
+    include: ['react-pdf', 'react'],
   },
   build: {
     commonjsOptions: {
-      include: [/react-pdf/, /pdfjs-dist/],
+      include: [/react-pdf/, /pdfjs-dist/, /node_modules/],
     },
   },
   resolve: {
